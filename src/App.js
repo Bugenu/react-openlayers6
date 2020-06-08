4c5907d40101ec5b0a198c300e20432aa0192ac4
Shane Richardson <srichardson1928@hotmail.co.uk>
2020-06-08 23:55:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+require("ol/ol.css");
+var ol_Map = require("ol/Map").default;
+var ol_layer_Tile = require("ol/layer/Tile").default;
+var ol_source_OSM = require("ol/source/OSM").default;
+var ol_View = require("ol/View").default;
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.map = {};
+  }
+
+  componentDidMount() {
+    this.map = new ol_Map({
+      target: "map",
+      layers: [
+        new ol_layer_Tile({
+          source: new ol_source_OSM()
+        })
+      ],
+      view: new ol_View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div id="map" className="map" />
+      </div>
+    );
+  }
 }
 
 export default App;
